f5c877e85d1d2667a8641464241bcf3e1770356a
Thomas Sparber <thomas@sparber.eu>
2015-04-01 07:02:41 +0000

    Added a reset counter to ClusterMutex to restart sleeping. Now the load is better distributed across the nodes

--- include/cluster/clustermutex.hpp
+++ include/cluster/clustermutex.hpp
@@ -1,3 +1,11 @@
+/**
+  *
+  * (C) Thomas Sparber
+  * thomas@sparber.eu
+  * 2013-2015
+  *
+ **/
+
 #ifndef MUTEX_HPP
 #define MUTEX_HPP
 
@@ -9,20 +17,79 @@ namespace cluster
 
 class Address;
 
+/**
+  * This enum defines the actions of the
+  * ClusterMutex
+ **/
+enum class ClusterMutexOperation : unsigned char
+{
+	/**
+	  * Locks the mutex
+	 **/
+	lock = 'l',
+
+	/**
+	  * Unlocks the mutex
+	 **/
+	unlock = 'u'
+};
+
+/**
+  * This function is overloaded from the Package class
+  * to retrieve a ClusterMutexOperation from a Package
+ **/
+template <>
+inline bool operator>>(const Package &p, ClusterMutexOperation &t)
+{
+	return p.getAndNext(reinterpret_cast<unsigned char&>(t));
+}
+
+
+/**
+  * This function is overloaded from the Package class
+  * to insert a ClusterMutexOperation into a Package
+ **/
+template <>
+inline void operator<<(Package &p, const ClusterMutexOperation &t)
+{
+	p.append(reinterpret_cast<const unsigned char&>(t));
+}
+
+/**
+  * The ClusterMutex can be used to synchronize
+  * access to an object e.g. ClusterContainer
+ **/
 class ClusterMutex : public ClusterObject
 {
 
 public:
-	ClusterMutex(ClusterObject *network, unsigned int ui_minSleepTime=10) :
+	/**
+	  * Constructs a mutex and adds it to the given network.
+	  * The minSleepTime determines the minimum sleep time
+	  * between retries when the mutex can't be locked.
+	  * ResetEvery determines the number of retries when
+	  * the sleeptime is reset to the minimum.
+	 **/
+	ClusterMutex(ClusterObject *network, unsigned int ui_minSleepTime=10, unsigned int ui_resetEvery=17) :
 		ClusterObject(network),
 		tryLock(false),
                 selfLocked(false),
                 clusterLocked(false),
-		minSleepTime(ui_minSleepTime)
+		minSleepTime(ui_minSleepTime),
+		resetEvery(ui_resetEvery)
 	{}
 
+	/**
+	  * Default destructor
+	 **/
 	virtual ~ClusterMutex() {}
 
+	/**
+	  * This function locks the mutex. It the mutex
+	  * can't be locked the function sleeps for a while
+	  * (exponentially more the often it fails to lock)
+	  * and retries.
+	 **/
 	void lock()
 	{
 		unsigned int counter = 0;
@@ -30,13 +97,19 @@ public:
 		{
 			if(clusterLocked)
 			{
-				//counter = 0;
 				while(clusterLocked)usleep(minSleepTime);
 			}
 			else usleep(int(pow(2, counter++) * minSleepTime));
+
+			//reset counter
+			if(counter >= resetEvery)counter = 0;
 		}
 	}
 
+	/**
+	  * This function tries to lock the mutex and
+	  * returns whether is was successful or not.
+	 **/
 	bool try_lock()
         {
 		if(selfLocked)return true;	//Mutex already locked
@@ -55,12 +128,23 @@ public:
 
 		//Sending the request to the cluster
 		Package response;
-		send('l', &response);
-		if(response.getLength() != 0)	//Mutex can't be locked if one clusternode returns answer
+		send(ClusterMutexOperation::lock, &response);
+
+		bool rejected = false;
+		unsigned char c;
+		while(response>>c)
 		{
-			//TODO only send unlock request to members who locked mutex
-			//Save bandwith!
-			send('u', &response);
+			if(c == 'x')
+			{
+				rejected = true;
+				break;
+			}
+		}
+
+		if(rejected)	//Mutex can't be locked if one clusternode returns answer
+		{
+			//Send unlock request
+			send(ClusterMutexOperation::unlock, &response);
 
 			tryLock = false;
 			return false;
@@ -72,32 +156,72 @@ public:
 		return true;
 	}
 
+	/**
+	  * This function unlocks the ClusterMutex
+	 **/
         void unlock()
         {
-                send('u');
+                send(ClusterMutexOperation::unlock);
                 selfLocked = false;
 	}
 
+	/**
+	  * Returns whether the mutex is locked
+	  * by the cluster or the current mutex
+	 **/
+	bool isLocked() const
+	{
+		return selfLocked || clusterLocked;
+	}
+
+	/**
+	  * Returns whether the mutex is locked
+	  * by the current mutex
+	 **/
+	bool isSelfLocked() const
+	{
+		return selfLocked;
+	}
+
+	/**
+	  * Returns whether the mutex is locked
+	  * by the cluster
+	 **/
+	bool isClusterLocked() const
+	{
+		return clusterLocked;
+	}
+
+	/**
+	  * Returns the type of ClusterObject
+	 **/
 	virtual std::string getType() const
 	{
 		return "Clustermutex";
 	}
 
-private:
+protected:
+	/**
+	  * This function is called for every Package
+	  * which is received for the ClusterMutex.
+	 **/
 	virtual bool received(const Address &/*ip*/, const Package &message, Package &answer, Package &/*to_send*/)
 	{
-		unsigned char type;
+		ClusterMutexOperation type;
 		message>>type;
 
 		switch(type)
 		{
-		case 'l':	//Lock
+		case ClusterMutexOperation::lock:	//Lock
+			//Locking is only allowed if the mutex is not
+			//already locked by the current mutex, the cluster
+			//or the current mutex is trying to lock it.
 			if(tryLock || selfLocked || clusterLocked)
 				answer<<'x';
 			else
 				clusterLocked = true;
 			return true;
-		case 'u':	//Unlock
+		case ClusterMutexOperation::unlock:	//Unlock
 			clusterLocked = false;
 			return true;
 		default:
@@ -106,10 +230,34 @@ private:
 	}
 
 private:
+	/**
+	  * A flag that indicates whether the current
+	  * mutex is trying to lock it
+	 **/
 	bool tryLock;
+
+	/**
+	  * A flag that indicates whether the mutex
+	  * is locked by the current mutex
+	 **/
 	bool selfLocked;
+
+	/**
+	  * A flag that indicates whether the mutex
+	  * is locked by the cluster
+	 **/
 	bool clusterLocked;
+
+	/**
+	  * The minimum sleep time between retries
+	 **/
 	unsigned int minSleepTime;
+
+	/**
+	  * The number of retries when the sleeptime
+	  * is reset to the minimum
+	 **/
+	unsigned int resetEvery;
 
 }; // end class ClusterMutex
 
